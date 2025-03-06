cce9ffbd4400296cd1d3cf8137f9b456546e81fc
sagarbindu <68044646+sagarbindu@users.noreply.github.com>
2025-03-06 14:24:52 +0530

    Update conditional.sh

--- conditional.sh
+++ conditional.sh
@@ -7,3 +7,19 @@ echo "Number is equal to 10"
 else
 echo "Number is less than 10"
 fi
+
+fruit="apple"
+case $fruit in
+"apple")
+echo "It's an apple"
+;;
+"banana")
+echo "It's a banana"
+;;
+"orange")
+echo "It's an orange"
+;;
+*)
+echo "Unknown fruit"
+;;
+esac
