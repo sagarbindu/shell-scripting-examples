cc477e763e9b019a89b8cb07320ee0cc5ae12c41
sagarbindu <68044646+sagarbindu@users.noreply.github.com>
2025-03-06 14:55:08 +0530

    Update function.sh

--- function.sh
+++ function.sh
@@ -15,3 +15,15 @@ echo "Second argument: $2"
 }
 # Call the print_args function with arguments
 print_args "Hello" "World"
+
+# Declare a function named calculate_sum
+calculate_sum() {
+local num1="$1"
+local num2="$2"
+local sum=$((num1 + num2))
+echo "$sum"
+return $sum
+}
+# Call the calculate_sum function and capture the output
+result=$(calculate_sum 10 20)
+echo "Sum: $result"
