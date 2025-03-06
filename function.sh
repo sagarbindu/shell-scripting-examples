c0e5402395fc8ad6be53df5f872a318762bbb9cd
sagarbindu <68044646+sagarbindu@users.noreply.github.com>
2025-03-06 14:53:45 +0530

    Update function.sh

--- function.sh
+++ function.sh
@@ -6,3 +6,12 @@ echo "Hello, how are you?"
 }
 # Call the greet function
 hello
+
+
+# Declare a function named print_args
+print_args() {
+echo "First argument: $1"
+echo "Second argument: $2"
+}
+# Call the print_args function with arguments
+print_args "Hello" "World"
