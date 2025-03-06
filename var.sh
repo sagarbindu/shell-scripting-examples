0cf0cfb7f835a5d8bf5b092c39d5b71d4f115713
sagarbindu <68044646+sagarbindu@users.noreply.github.com>
2025-03-06 14:07:47 +0530

    Update var.sh

--- var.sh
+++ var.sh
@@ -25,6 +25,21 @@ string="Shell Scripting"
 length=${#string}
 echo "Length: $length" # Output: Length: 15
 
+variable="Hello World"
+echo "Using double quotes: $variable"
+echo 'Using single quotes: $variable'
+echo Using no quotes: $variable
+echo "Using double quotes: '$variable'"
+
+# Using Backticks
+current_date=`date`
+echo "Current date: $current_date"
+# Using $(...)
+current_time=$(date +%H:%M:%S)
+echo "Current time: $current_time"
+
+
+
 # Substring Extraction
 substring=${string:0:4} # Extracts first 4 characters
 echo "Substring: $substring" # Output: Substring: Shell
