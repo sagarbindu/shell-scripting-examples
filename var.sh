bf38d866c19c6a52943da06a028dc897ef38ba22
sagarbindu <dashsagarbindu789@gmail.com>
2025-03-06 13:38:11 +0530

    add some codes

--- var.sh
+++ var.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 name="sagar"
 echo "Hi, $name"
+
+age=24
+echo "Age: $age years"
+
+x=4
+y=5
+
+sum=$((x+y))
+
+echo "Sum:$sum"
+
+Current_date=`date` 
+echo "Current Date & Time : $Current_date"
+
+readonly pi=3.1415
+pi=4.33
+echo "$pi"
+
+
+echo "Please enter your name"
+read name
+echo "Hello, $name !, nice to meet u"
