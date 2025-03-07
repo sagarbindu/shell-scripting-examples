83dea7e5e5ab1bbcf206cbace0792f7b5a194c50
sagarbindu <dashsagarbindu789@gmail.com>
2025-03-07 12:05:36 +0530

    deleted some files

--- text-processing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Read the text file line by line
-while IFS='|' read -r name age profession; do
-echo "Name: $name"
-echo "Age: $age"
-echo "Profession: $profession"
-echo "---"
-done < data.txt
